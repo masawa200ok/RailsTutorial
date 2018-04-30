6066b537055800ba5a4835a234287ddef2070ebb
masawa200ok <project.masawa@gmail.com>
2018-04-30 20:16:21 +0900

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy,
+                                        :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: [:destroy]
 
@@ -54,6 +55,20 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render "show_follow"
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render "show_follow"
+  end
+
   private
 
     # Strong Prameters
@@ -62,16 +77,6 @@ class UsersController < ApplicationController
         :name, :email, :password, :password_confirmation
       )
     end
-
-    # before_action
-    # ログイン済みユーザかどうか確認
-    # def logged_in_user
-    #  unless logged_in?
-    #    store_location
-    #    flash[:danger] = "Please log in."
-    #    redirect_to login_url
-    #  end
-    # end
 
     # 正しいユーザかどうか確認（正しくない場合root_urlへ） 
     def correct_user
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# ユーザー
 User.create!(name: "Example User",
              email: "example@railstutorial.org",
-             password: "foobar",
-             password_confirmation: "foobar",
+             password: "password",
+             password_confirmation: "password",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
@@ -27,9 +28,18 @@ User.create!(name: "Example User",
                activated_at: Time.zone.now)
 end
 
+# マイクロポスト
 users = User.order(:created_at).take(6)
 50.times do |n|
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# リレーションシップ
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
 
